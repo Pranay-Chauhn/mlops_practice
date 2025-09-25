ccf0e3856d27964e7b8e516364d3bde1ee865088
Pranay-Chauhn <pranaychauhan663@gmail.com>
2025-09-25 23:09:56 +0530

    basic experiment tracking with mlflow

--- src/training.py
+++ src/training.py
@@ -1,2 +1,17 @@
 # Model Training
-print("Hello World")
+import mlflow 
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
+with mlflow.start_run() :
+    model = RandomForestClassifier()
+    model.fit(X_train,y_train)
+    prediction = model.predict(X_test)
+    accuracy = accuracy_score(y_test, prediction)
+
+    mlflow.log_metric("accuracy", accuracy)
+    
