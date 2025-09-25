043f11227769da26b3dde6da017e8ba0bcf3db2a
Pranay-Chauhn <pranaychauhan663@gmail.com>
2025-09-25 23:51:47 +0530

    Create a Basic Monitoring Setup

--- src/training.py
+++ src/training.py
@@ -1,17 +1,33 @@
 # Model Training
 import mlflow 
+import logging
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s - %(levelname)s - %(message)s',
+    handlers = [
+        logging.FileHandler("mlops.log"),
+        logging.StreamHandler()
+    ]
+)
+logging.info("Starting model training process....")
+
+logging.info("Loading Data...")
 iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
+logging.info("Data Loaded and Split into training and test sets.")
+
 with mlflow.start_run() :
+    logging.info("Training the Random Forest Model")
     model = RandomForestClassifier()
     model.fit(X_train,y_train)
+    logging.info("Training Completed...")
     prediction = model.predict(X_test)
     accuracy = accuracy_score(y_test, prediction)
 
     mlflow.log_metric("accuracy", accuracy)
-    
+    logging.info(f"Model Accuracy : {accuracy:.4f}")
